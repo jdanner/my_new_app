c6a7e4644d23f70ec9b65fef3fe0ad64d3bd0489
John Danner <john@danners.org>
2024-11-03 08:30:17 -0800

    migrations------

--- my_new_app/__init__.py
+++ my_new_app/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
-from my_new_app.extensions import db, migrate, bcrypt
+from my_new_app.extensions import db, migrate, bcrypt, login_manager
 
 app = Flask(__name__)
 
 # Configure the app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['SECRET_KEY'] = 'your-secret-key'  # Make sure this is set
+app.config['SECRET_KEY'] = 'your-secret-key'
 
 # Initialize extensions
 db.init_app(app)
 migrate.init_app(app, db)
 bcrypt.init_app(app)
+login_manager.init_app(app)
 
-# Import routes at the bottom to avoid circular imports
+# Import routes at the bottom
 from my_new_app import routes
