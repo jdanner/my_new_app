9a73edfbbe6f7cb12c0a5874c7d2233b5a3339cb
John Danner <john@danners.org>
2024-11-03 08:26:38 -0800

    migrations-----

--- build.sh
+++ build.sh
@@ -1,15 +1,8 @@
 #!/usr/bin/env bash
-# Make script executable: chmod +x build.sh
 
 # Install Python dependencies
 pip install -r requirements.txt
 
-# Create the database tables first
-python init_db.py
-
-# Then run migrations
+# Just run migrations - this will create tables too
 export FLASK_APP=wsgi.py
-flask db stamp head  # Mark current migration as complete
-flask db upgrade    # Run any pending migrations
-
-# You might also want to add any other build steps here
+flask db upgrade
